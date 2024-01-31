e50fa7bf0561e1c27ff6ac2b4f13497b1f83d955
Suresh-uta <sxk0703@mavs.uta.edu>
2024-01-30 19:47:22 -0600

    created Rest Apis

--- Backend/src/main/java/controller/EmployeeController.java
+++ Backend/src/main/java/controller/EmployeeController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import repository.EmployeeRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping("/pemployees")
+    public ResponseEntity addExpense(@RequestBody Employee employee) {
+        employeeRepository.save(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
 }
+
+
--- Backend/src/main/java/repository/EmployeeRepository.java
+++ Backend/src/main/java/repository/EmployeeRepository.java
@@ -1,4 +1,10 @@
 package repository;
 
-public interface EmployeeRepository {
+import model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
 }
